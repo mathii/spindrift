8593879371d7c6a212b901affa9f2201393d48d2
Iain Mathieson <iain_mathieson@hms.harvard.edu>
2015-01-31 14:48:57 -0500

    5 not 3

--- snp_data.py
+++ snp_data.py
@@ -201,7 +201,7 @@ class frequency_data(snp_data):
         file_pops=np.array(header.split()[3:])
         which_pops=np.in1d(file_pops, np.array(pops))
         self.pops=list(file_pops[which_pops])
-        self.freq=np.loadtxt(freq_file, dtype=float, usecols=3+np.where(which_pops)[0])
+        self.freq=np.loadtxt(freq_file, dtype=float, usecols=5+np.where(which_pops)[0])
 
         freq_file.seek(0)
         freq_file.next()
